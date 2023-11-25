3e1f6d4ef405d388b0b34d679fc88963159008e8
sudib12 <117530546+sudib12@users.noreply.github.com>
2023-11-25 17:22:49 +0000

    dat

---
+++ basics/datatypes.js
@@ -0,0 +1,5 @@
+"use strict"; //treat all JS code as newer versionj
+let age= 21
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
---
+++ basics/variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553 //for declaring constant
+let accontEmail = "sudib@google.com" //for declaring variables
+var accountPassword = "12345"  //for declaring variables(not used)
+accountCity = "Jaipur"
+
+// accountId = 2  // Constant can't be modified 
+
+accountPassword = "21212121"
+accontEmail = "hc@hc.com"
+accountCity = "Bengaluru"
+let accountState;
+console.log(accountId);
+console.table([accountId,accontEmail,accountPassword,accountCity,accountState])
+
+/*
+comment ese likhte hai 
+*/
