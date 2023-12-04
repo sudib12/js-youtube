4a02dc6acc68249e09f1ac835dcd084893d74328
sudib12 <117530546+sudib12@users.noreply.github.com>
2023-12-04 20:19:32 +0000

    conversion of data types

---
+++ basics/conversionoperation.js
@@ -0,0 +1,22 @@
+let score="sudib"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber=Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber)
+
+let logged=1
+let boolenlogged=Boolean(logged)
+//console.log(typeof boolenlogged);
+//console.log(boolenlogged);
+
+let someNumber=33 
+let stringnumber=String(someNumber)
+console.log(stringnumber)
+console.log(typeof stringnumber)
+
+
+
+//NaN is not a number
